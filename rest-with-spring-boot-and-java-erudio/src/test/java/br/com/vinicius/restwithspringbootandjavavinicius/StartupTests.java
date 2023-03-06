b758e304ef98e892a8d95dd4032200fc3ba34d82
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2023-03-06 16:14:27 -0300

    feat: create project spring

---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/vinicius/restwithspringbootandjavavinicius/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.restwithspringbootandjavavinicius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
