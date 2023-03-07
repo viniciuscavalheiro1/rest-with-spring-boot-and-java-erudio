55954d4190e4c5c84ed4eb9f998d2ad111269415
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2023-03-07 14:05:53 -0300

    chore: refactory class converter

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/vinicius/services/MathService.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/vinicius/services/MathService.java
@@ -1,5 +1,6 @@
 package br.com.vinicius.services;
 
+import br.com.vinicius.converters.NumberConverter;
 import br.com.vinicius.exceptions.UnsupportedMathOperationException;
 import org.springframework.stereotype.Service;
 
@@ -7,49 +8,37 @@ import org.springframework.stereotype.Service;
 public class MathService {
 
     public Double Soma(String numberOne, String numberTwo) throws Exception {
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Please set a numeric value!");
         }
-        return ConvertToDouble(numberOne) * ConvertToDouble(numberTwo);
+        return NumberConverter.ConvertToDouble(numberOne) * NumberConverter.ConvertToDouble(numberTwo);
     }
 
     public Double Subtracao(String numberOne, String numberTwo) throws Exception {
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Please set a numeric value!");
         }
-        return ConvertToDouble(numberOne) / ConvertToDouble(numberTwo);
+        return NumberConverter.ConvertToDouble(numberOne) / NumberConverter.ConvertToDouble(numberTwo);
     }
 
     public Double Multiplicacao (String numberOne, String numberTwo) throws Exception {
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Please set a numeric value!");
         }
-        return ConvertToDouble(numberOne) + ConvertToDouble(numberTwo);
+        return NumberConverter.ConvertToDouble(numberOne) + NumberConverter.ConvertToDouble(numberTwo);
     }
 
     public Double Divisao (String numberOne, String numberTwo) throws Exception {
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Please set a numeric value!");
         }
-        return ConvertToDouble(numberOne) + ConvertToDouble(numberTwo);
+        return NumberConverter.ConvertToDouble(numberOne) + NumberConverter.ConvertToDouble(numberTwo);
     }
 
     public Double RaizQuadrada (String number) throws Exception {
-        if(!isNumeric(number)) {
+        if(!NumberConverter.isNumeric(number)) {
             throw new UnsupportedMathOperationException("Please set a numeric value!");
         }
         return Math.sqrt(Double.parseDouble(number));
-    }
-
-    private Double ConvertToDouble(String strNumber) {
-        if (strNumber == null) return 0D;
-        String number = strNumber.replaceAll(",", ".");
-        if(isNumeric(number)) return Double.parseDouble(number);
-        return 0D;
-    }
-    private boolean isNumeric(String strNumber) {
-        if (strNumber == null) return false;
-        String number = strNumber.replaceAll(",", ".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
